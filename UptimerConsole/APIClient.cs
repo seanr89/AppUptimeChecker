4f8efba36997a0a63cbad4c3b76d81bdad9d73ee
seanr89 <srafferty89@gmail.com>
2018-10-06 16:25:10 +0100

    Content copied over from VSTS to git - untested at this time

--- UptimerConsole/APIClient.cs
+++ UptimerConsole/APIClient.cs
@@ -13,6 +13,7 @@ public class APIClient
     /// Constructor
     /// </summary>
     /// <param name="apiurl">API Response URL</param>
+    /// <param name="url"></param>
     public APIClient(string apiurl, string url)
     {
         _APIURL = apiurl;
@@ -21,5 +22,153 @@ public class APIClient
         _URLID = 0;
         _IsActive = true;
         TestClient();
+    }
+
+    /// <summary>
+    /// Method to run and check if the API Client is active and available
+    /// Will then set the parameter to handle if the API is running
+    /// </summary>
+    async void TestClient()
+    {
+        try
+        {
+            using(HttpClient client = new HttpClient())
+            {
+                client.Timeout = TimeSpan.FromSeconds(5.0);
+                client.DefaultRequestHeaders.Accept.Clear();
+                
+                var response = await client.GetAsync(_APIURL+"/api/Home/Available");
+
+                if(response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("TestClient", "API Client is Active");
+                    _IsActive = true;
+                }
+                // else
+                // {
+                //     Console.WriteLine("TestClient", "API Client is Not Active");
+                //     //_IsActive = false;
+                // }
+            }
+        }
+        catch(HttpRequestException hre)
+        {
+            Console.WriteLine($"TestClient HttpRequestException Caught {hre.Message}");
+        }
+        catch(Exception e)
+        {
+            Console.WriteLine($"Exception Caught {e.Message}");
+        }
+    }
+
+    /// <summary>
+    /// Method to handle the saving of Uptime Ping responses to an API
+    /// </summary>
+    /// <param name="response">The log response object to be sent</param>
+    public async Task SaveResponse(LogResponse response)
+    {
+        Console.WriteLine($"SaveResponse at {response.EventDate.ToLongTimeString()}");
+        try
+        {
+            if(_IsActive == false)
+            {
+                return;
+            }
+            //implement the using statement to ensure the client is closed
+            using(HttpClient client = new HttpClient())
+            {
+                AppendURLIDTOLogResponse(response, _URLID);
+                string jsonResponse = JsonConvert.SerializeObject(response);
+                HttpContent _Body = new StringContent(jsonResponse);
+
+                // and add the header to this object instance
+                // optional: add a formatter option to it as well
+                _Body.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
+
+                client.Timeout = TimeSpan.FromSeconds(5.0);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                var APIResponse = await client.PostAsync(_APIURL+"/api/Uptime/Post", _Body);
+                if(APIResponse != null)
+                {
+                    Console.WriteLine($"Handling API Response with status: {APIResponse.StatusCode}");
+                }
+            }
+        }
+        catch(HttpRequestException hre)
+        {
+            Console.WriteLine($"SaveResponse HttpRequestException Caught {hre.Message}");
+        }
+        catch(Exception e)
+        {
+            Console.WriteLine($"Exception Caught {e.Message}");
+        }
+        
+    }
+
+        /// <summary>
+    /// Operation to request initialise and request the UniqueID of the URL
+    /// </summary>
+    /// <returns></returns>
+    public async Task InitialiseURLToAPI()
+    {
+        //Console.WriteLine("Executing: APIClient:InitialiseURLToAPI");
+        if(_IsActive)
+        {
+            try
+            {
+                //implement the using statement to ensure the client is closed
+                using(HttpClient client = new HttpClient())
+                {
+                    //Console.WriteLine($"Url is {_APIURL}/api/URL/ReadIDForURL/");
+                    client.Timeout = TimeSpan.FromSeconds(5.0);
+                    client.DefaultRequestHeaders.Accept.Clear();
+                    // Construct an HttpContent from a StringContent
+                    HttpContent _Body = new StringContent(JsonConvert.SerializeObject(_URL));
+                    // and add the header to this object instance
+                    // optional: add a formatter option to it as well
+                    _Body.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
+                    
+                    var APIResponse = await client.PostAsync(_APIURL+"/api/URL/ReadIDForURL", _Body);
+                    if(APIResponse != null && APIResponse.IsSuccessStatusCode)
+                    {
+                        //request the returned content
+                        var contents = await APIResponse.Content.ReadAsStringAsync();
+                        ReadURLIDFromResponse(contents);
+                    }
+                    // else
+                    // {
+                    //     Console.WriteLine($"No response or unsuccessful for method call: InitialiseURLToAPI with response {APIResponse.StatusCode}");
+                    // }
+                }
+            }
+            catch(HttpRequestException hre)
+            {
+                Console.WriteLine($"InitialiseURLToAPI HttpRequestException Caught {hre.Message}");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine($"InitialiseURLToAPI: Exception caught {e.Message}");
+            }
+        }
+    }
+
+    /// <summary>
+    /// Operation to parse out the URL contents to set the _URLID
+    /// </summary>
+    /// <param name="contents">Return parameter content from httpclient event</param>
+    private void ReadURLIDFromResponse(string contents)
+    {
+        _URLID = Convert.ToInt32(contents);
+    }
+
+    /// <summary>
+    /// Operation to append on the url ID to the LogReponse
+    /// </summary>
+    /// <param name="response">LogReponse object to append the URL ID onto</param>
+    /// <param name="ID"></param>
+    private void AppendURLIDTOLogResponse(LogResponse response, int ID)
+    {
+        response.URLID = ID;
     }
 }
--- UptimerConsole/Program.cs
+++ UptimerConsole/Program.cs
@@ -4,9 +4,46 @@ class UptimerConsole
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+        //initialise parameters with defaults
+        _Url = "https://www.google.com";
+        _Frequency = "15000";
+
+        if(args.Any())
+        {
+            _Url = args[0] ?? "https://www.google.com";
+            _Frequency = args[1] ?? "15000";
+        }
+
+        //initialise the API Client with API Url included to the AzureAPI
+        _APIClient = new APIClient("https://siteuptimeapi.azurewebsites.net", _Url);
+        //_APIClient = new APIClient("http://localhost:5000", _Url);
+        _APIClient.InitialiseURLToAPI();
+
+        //Console.WriteLine("Starting URL Pinging");
+        
+        //Start a timer to ping the application URL
+        var timer = new Timer(async (s)=> await PingUrl(s), null, 3000, Convert.ToInt32(_Frequency));
+        Console.ReadLine();
     }
 
+    /// <summary>
+    /// Method handle the pinging of the url, handling of message response
+    /// and the saving of the response
+    /// </summary>
+    /// <param name="state"></param>
+    /// <returns></returns>
+    private static async Task PingUrl(object state)
+    {
+        Console.WriteLine($"PingUrl with current time {DateTime.Now.ToLongTimeString()}");
+        var stopwatch = Stopwatch.StartNew();
+        var response = await GetResponse();
+        if (response != null)
+        {
+            LogResponse logResponse = CreateLogResponse(response.StatusCode, (int)stopwatch.ElapsedMilliseconds, response);
+            await _APIClient.SaveResponse(logResponse);
+            stopwatch.Stop();
+        }
+    }
 
     /// <summary>
     /// ASYNC Request the response from the provided URL on startup
@@ -28,5 +65,23 @@ class UptimerConsole
             Console.WriteLine("GetResponse:Exception caught: " + ex.StackTrace);
             return null;
         }
+    }
+
+    /// <summary>
+    /// Method operation to handle the creation of a log response for saving to the API
+    /// </summary>
+    /// <param name="responseCode"></param>
+    /// <param name="duration"></param>
+    /// <param name="message"></param>
+    /// <returns>A LogResponse object</returns>
+    private static LogResponse CreateLogResponse(HttpStatusCode responseCode, int duration, HttpResponseMessage message)
+    {
+        LogResponse result = new LogResponse();
+        //result.URL = _Url;
+        result.Duration = duration;
+        result.ResponseCode = responseCode;
+        result.EventDate = DateTime.Now;
+        result.Response = message;
+        return result;
     }
 }
