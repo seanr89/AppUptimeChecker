2bb3684af00d719645ee951cd054bd29a29ccf6f
seanr89 <srafferty89@gmail.com>
2018-12-15 21:35:58 +0000

    Upgrading dockerfile and netcore to 2.2 along with a HostedService to take over records

--- UptimerConsole/Program.cs
+++ UptimerConsole/Program.cs
@@ -8,18 +8,33 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Blob;
 
 class UptimerConsole
 {
-    private static string _Url;
-    private static string _Frequency;
-    private static APIClient _APIClient;
-    private static Timer _Timer;
-    static AutoResetEvent _autoEvent = null;
-    private static HttpClient _Client;
+    // private static string _Url;
+    // private static string _Frequency;
+    private TimerService _TimerService;
     
+    public static async Task Main(string[] args)
+    {
+        var hostBuilder = new HostBuilder()
+             // Add configuration, logging, ...
+            .ConfigureServices((hostContext, services) =>
+            {
+                // Add your services with depedency injection.
+                services.AddHostedService<TimerService>();
+            });
+
+        await hostBuilder.RunConsoleAsync();
+        Console.WriteLine($"Application Complete!");
+        Console.ReadLine();
+    }
+
+/* 
     static void Main(string[] args)
     {
         //initialise parameters with defaults
@@ -32,38 +47,42 @@ class UptimerConsole
             _Frequency = args[1] ?? "30000";
         }
 
+        var builder = new HostBuilder(); // from Microsoft.Extensions.Hosting
+
         //initialise the API Client with API Url included to the AzureAPI
-        _APIClient = new APIClient("https://siteuptimeapi.azurewebsites.net", _Url);
-        _APIClient.InitialiseURLToAPI();
+        // _APIClient = new APIClient("https://siteuptimeapi.azurewebsites.net", _Url);
+        // _APIClient.InitialiseURLToAPI();
 
         Console.WriteLine("Starting Timer in 10 secs");
         _Client = new HttpClient();
         _Client.Timeout = TimeSpan.FromSeconds(10.0);
         
         //StartTimer();
-        try{
-            _autoEvent = new AutoResetEvent(false);
-            //ensure current timer is stopped
-            _Timer = null;
-            //Start a timer to ping the application URL
-            _Timer = new Timer(async (s)=> await PingUrl(s), _autoEvent, 10000, Convert.ToInt32(_Frequency));
-            //GC.KeepAlive(_Timer);
-        }
-        catch(System.OutOfMemoryException e)
-        {
-            Console.WriteLine($"Application Timer has had a memory issue : {e.Message}");
-            Environment.Exit(0);
-            AppDomain.CurrentDomain.ProcessExit += (sender, eventArgs) => Console.WriteLine("proc exit");
-        }
-        catch(System.Threading.Tasks.TaskCanceledException et)
-        {
-            Console.WriteLine($"Application Timer has cancelled for exception : {et.Message}");
-            Environment.Exit(0);
-            AppDomain.CurrentDomain.ProcessExit += (sender, eventArgs) => Console.WriteLine("proc exit");
-        }
+ //       try{
+            // _autoEvent = new AutoResetEvent(false);
+            // //ensure current timer is stopped
+            // _Timer = null;
+            // //Start a timer to ping the application URL
+            // _Timer = new Timer(async (s)=> await PingUrl(s), _autoEvent, 10000, Convert.ToInt32(_Frequency));
+            // //GC.KeepAlive(_Timer);
+        // }
+        // catch(System.OutOfMemoryException e)
+        // {
+        //     Console.WriteLine($"Application Timer has had a memory issue : {e.Message}");
+        //     Environment.Exit(0);
+        //     AppDomain.CurrentDomain.ProcessExit += (sender, eventArgs) => Console.WriteLine("proc exit");
+        // }
+        // catch(System.Threading.Tasks.TaskCanceledException et)
+        // {
+        //     Console.WriteLine($"Application Timer has cancelled for exception : {et.Message}");
+        //     Environment.Exit(0);
+        //     AppDomain.CurrentDomain.ProcessExit += (sender, eventArgs) => Console.WriteLine("proc exit");
+        // }
         Console.WriteLine($"Application Complete!");
         Console.ReadLine();
     }
+
+    */
 
     /// <summary>
     /// Method handle the pinging of the url, handling of message response
@@ -71,57 +90,57 @@ class UptimerConsole
     /// </summary>
     /// <param name="state"></param>
     /// <returns></returns>
-    private static async Task PingUrl(object state)
-    {
-        //Console.WriteLine($"PingUrl with current time {DateTime.Now.ToLongTimeString()}");
-        var watch = Stopwatch.StartNew();
-        var response = await GetResponse();
-        if (response != null)
-        {
-            LogResponse logResponse = CreateLogResponse(response.StatusCode, (int)watch.ElapsedMilliseconds, response);
-            await _APIClient.SaveResponse(logResponse);
-            logResponse = null;
-        }
-        response = null;
-        watch.Stop();
-        watch = null;
-        GC.KeepAlive(_Timer);
-        // Force a garbage collection to occur for this demo.
-        //GC.Collect();
-    }
+    // private static async Task PingUrl(object state)
+    // {
+    //     //Console.WriteLine($"PingUrl with current time {DateTime.Now.ToLongTimeString()}");
+    //     var watch = Stopwatch.StartNew();
+    //     var response = await GetResponse();
+    //     if (response != null)
+    //     {
+    //         LogResponse logResponse = CreateLogResponse(response.StatusCode, (int)watch.ElapsedMilliseconds, response);
+    //         await _APIClient.SaveResponse(logResponse);
+    //         logResponse = null;
+    //     }
+    //     response = null;
+    //     watch.Stop();
+    //     watch = null;
+    //     GC.KeepAlive(_Timer);
+    //     // Force a garbage collection to occur for this demo.
+    //     //GC.Collect();
+    // }
 
-    /// <summary>
-    /// ASYNC Request the response from the provided URL on startup
-    /// </summary>
-    /// <returns>HttpResponseMessage if return accepted</returns>
-    private static async Task<HttpResponseMessage> GetResponse()
-    {
-        try
-        {
-            var request = new HttpRequestMessage(HttpMethod.Get, _Url);
-            return await _Client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine("GetResponse : Exception caught: " + ex.Message);
-            return null;
-        }
-    }
+    // /// <summary>
+    // /// ASYNC Request the response from the provided URL on startup
+    // /// </summary>
+    // /// <returns>HttpResponseMessage if return accepted</returns>
+    // private static async Task<HttpResponseMessage> GetResponse()
+    // {
+    //     try
+    //     {
+    //         var request = new HttpRequestMessage(HttpMethod.Get, _Url);
+    //         return await _Client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
+    //     }
+    //     catch (Exception ex)
+    //     {
+    //         Console.WriteLine("GetResponse : Exception caught: " + ex.Message);
+    //         return null;
+    //     }
+    // }
 
-    static void StartTimer()
-    {
-        try{
-            //ensure current timer is stopped
-            _Timer = null;
-            //Start a timer to ping the application URL
-            _Timer = new Timer(async (s)=> await PingUrl(s), null, 10000, Convert.ToInt32(_Frequency));
-        }
-        catch(System.OutOfMemoryException e)
-        {
-            Console.WriteLine($"Application Timer has had a memory issue : {e.Message}");
-            StartTimer();
-        }
-    }
+    // static void StartTimer()
+    // {
+    //     try{
+    //         //ensure current timer is stopped
+    //         _Timer = null;
+    //         //Start a timer to ping the application URL
+    //         _Timer = new Timer(async (s)=> await PingUrl(s), null, 10000, Convert.ToInt32(_Frequency));
+    //     }
+    //     catch(System.OutOfMemoryException e)
+    //     {
+    //         Console.WriteLine($"Application Timer has had a memory issue : {e.Message}");
+    //         StartTimer();
+    //     }
+    // }
 
     /// <summary>
     /// Method operation to handle the creation of a log response for saving to the API
@@ -130,14 +149,14 @@ class UptimerConsole
     /// <param name="duration"></param>
     /// <param name="message"></param>
     /// <returns>A LogResponse object</returns>
-    static LogResponse CreateLogResponse(HttpStatusCode responseCode, int duration, HttpResponseMessage message)
-    {
-        LogResponse result = new LogResponse();
-        //result.URL = _Url;
-        result.Duration = duration;
-        result.ResponseCode = responseCode;
-        result.EventDate = DateTime.Now;
-        result.Response = message;
-        return result;
-    }
+    // static LogResponse CreateLogResponse(HttpStatusCode responseCode, int duration, HttpResponseMessage message)
+    // {
+    //     LogResponse result = new LogResponse();
+    //     //result.URL = _Url;
+    //     result.Duration = duration;
+    //     result.ResponseCode = responseCode;
+    //     result.EventDate = DateTime.Now;
+    //     result.Response = message;
+    //     return result;
+    // }
 }
--- UptimerConsole/TimerService.cs
+++ UptimerConsole/TimerService.cs
@@ -1,19 +1,111 @@
 
+using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
 
-public class TimerService : IHostedService
+public class TimerService : IHostedService, IDisposable
 {
-    private Timer _timer;
+    private Timer _Timer;
+    private APIClient _ApiClient;
+    private string _Url;
+    private string _Frequency;
+    private readonly HttpClient _Client;
+
+    public TimerService()
+    {
+        _ApiClient = new APIClient("https://siteuptimeapi.azurewebsites.net", "https://www.google.com");
+        _ApiClient.InitialiseURLToAPI();
+
+        _Client = new HttpClient();
+    }
 
     public Task StartAsync(CancellationToken cancellationToken)
     {
+        _Timer = new Timer(DoWork, null, 10000, 
+            30000);
+            
         return Task.CompletedTask;
     }
 
     public Task StopAsync(CancellationToken cancellationToken)
     {
+        _Timer.Dispose();
+        _Timer = null;
         return Task.CompletedTask;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="state"></param>
+    private void DoWork(object state)
+    {
+        Console.WriteLine("Timed Background Service is working.");
+    }
+
+        /// <summary>
+    /// Method handle the pinging of the url, handling of message response
+    /// and the saving of the response
+    /// </summary>
+    /// <param name="state"></param>
+    /// <returns></returns>
+    private async Task PingUrl(object state)
+    {
+        //Console.WriteLine($"PingUrl with current time {DateTime.Now.ToLongTimeString()}");
+        var watch = Stopwatch.StartNew();
+        var response = await GetResponse();
+        if (response != null)
+        {
+            LogResponse logResponse = CreateLogResponse(response.StatusCode, (int)watch.ElapsedMilliseconds, response);
+            await _ApiClient.SaveResponse(logResponse);
+        }
+        GC.KeepAlive(_Timer);
+        // Force a garbage collection to occur for this demo.
+        //GC.Collect();
+    }
+
+        /// <summary>
+    /// ASYNC Request the response from the provided URL on startup
+    /// </summary>
+    /// <returns>HttpResponseMessage if return accepted</returns>
+    private async Task<HttpResponseMessage> GetResponse()
+    {
+        try
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, _Url);
+            return await _Client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("GetResponse : Exception caught: " + ex.Message);
+            return null;
+        }
+    }
+
+        /// <summary>
+    /// Method operation to handle the creation of a log response for saving to the API
+    /// </summary>
+    /// <param name="responseCode"></param>
+    /// <param name="duration"></param>
+    /// <param name="message"></param>
+    /// <returns>A LogResponse object</returns>
+    static LogResponse CreateLogResponse(HttpStatusCode responseCode, int duration, HttpResponseMessage message)
+    {
+        LogResponse result = new LogResponse();
+        //result.URL = _Url;
+        result.Duration = duration;
+        result.ResponseCode = responseCode;
+        result.EventDate = DateTime.Now;
+        result.Response = message;
+        return result;
+    }
+
+    public void Dispose()
+    {
+        _Timer?.Dispose();
     }
 }
