0991f14b7db2b19c43a27549c9b24cb27c21e23c
seanr89 <srafferty89@gmail.com>
2018-12-20 22:40:05 +0000

    playing with sample output content for sqlConnections

--- nodeAPI/Connections/sqlConnection.js
+++ nodeAPI/Connections/sqlConnection.js
@@ -20,5 +20,30 @@ var Connection = require('tedious').Connection;
         executeStatement();  
     });  
 
+    var Request = require('tedious').Request;  
+    //var TYPES = require('tedious').TYPES; 
+
     function executeStatement() {  
+        let request = new Request("SELECT c.CustomerID, c.CompanyName,COUNT(soh.SalesOrderID) AS OrderCount FROM SalesLT.Customer AS c LEFT OUTER JOIN SalesLT.SalesOrderHeader AS soh ON c.CustomerID = soh.CustomerID GROUP BY c.CustomerID, c.CompanyName ORDER BY OrderCount DESC;", function(err) {  
+            if (err) {  
+                console.log(err);}  
+            }); 
+
+            let result = '';  
+            request.on('row', function(columns) {  
+                columns.forEach(function(column) {  
+                  if (column.value === null) {  
+                    console.log('NULL');  
+                  } else {  
+                    result+= column.value + ' ';  
+                  }  
+                });  
+                console.log(result);  
+                result = '';  
+            });  
+    
+            request.on('done', function(rowCount, more) {  
+                console.log(rowCount + ' rows returned');  
+            });  
+            connection.execSql(request); 
     }
